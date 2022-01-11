b5d0f3fc89853e21e63337301a4914b5b4e0ae70
rihapradhan <75767843+rihapradhan@users.noreply.github.com>
2022-01-11 20:08:42 +0545

    Add files via upload

---
+++ assignments/assignment6/BoxLayout/boxLayout.java
@@ -0,0 +1,81 @@
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import javax.swing.BoxLayout.*;
+
+
+class boxLayout implements ActionListener{
+  
+        
+        JFrame f=new JFrame("Frame with association");
+        JLabel l1=new JLabel("Number1:");
+        JLabel l2=new JLabel("Number2:");
+        JLabel l3=new JLabel("Result:");
+        JTextField t1=new JTextField();
+        JTextField t2=new JTextField();
+        JTextField t3=new JTextField();
+        JButton b1=new JButton("ADD");
+        JButton b2=new JButton("SUB");
+        
+     boxLayout()
+       {
+        
+       
+         l1.setBounds(50, 50, 70, 30);
+         l2.setBounds(50,100,70,30);
+         l3.setBounds(50,150,70,30);
+         t1.setBounds(150, 50, 400, 30);
+       
+         t2.setBounds(150, 100, 400, 30);
+         t3.setBounds(150, 150, 400, 30);
+
+          b1 .setBounds(50, 180, 50, 30);
+		  b2.setBounds(102, 180, 50, 30);
+        f.setSize(500,500);
+        f.add(l1);
+        f.add(t1);
+        f.add(l2);
+        f.add(t2);
+       
+        f.add(b1);
+        f.add(b2);
+        f.add(l3);
+        f.add(t3);
+    
+        
+         f.setLayout(new BoxLayout(f.getContentPane(),BoxLayout.Y_AXIS));
+        
+        f.setVisible(true);
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        
+       
+        
+    }
+   
+        public void actionPerformed(ActionEvent event)  {
+        
+               int Number1=Integer.parseInt(t1.getText());
+               int Number2=Integer.parseInt(t2.getText());
+           
+               if(event.getSource()==b1){
+                   t3.setText(String.valueOf(Number1+Number2));
+
+               }else if(event.getSource()==b2){
+                       t3.setText(String.valueOf(Number1-Number2));
+                   }
+                  
+}
+              
+           
+           public static void main(String[] args) {
+               new boxLayout();
+           }
+           
+        
+       }
---
+++ assignments/assignment6/README.md
@@ -0,0 +1,2 @@
+## assigned date: jan 11 deadline : jan 12
+## submission date: jan 11 
